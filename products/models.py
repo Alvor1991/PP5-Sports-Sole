264391f963b3d840622d6d292a320ed6bb93fca7
Ryan Nolan <ryannolan_@hotmail.com>
2024-09-09 15:06:24 +0000

    Added search functionality

--- products/models.py
+++ products/models.py
@@ -1,17 +1,31 @@
 from django.db import models
 
+from django.db import models
+
 class Category(models.Model):
+    """
+    Represents a product category.
+    """
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
+        """
+        Returns the internal name of the category.
+        """
         return self.name
 
     def get_friendly_name(self):
+        """
+        Returns the user-friendly name of the category.
+        """
         return self.friendly_name
 
 
 class Product(models.Model):
+    """
+    Represents a product in the store.
+    """
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
@@ -21,4 +35,7 @@ class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
+        """
+        Returns the name of the product.
+        """
         return self.name
