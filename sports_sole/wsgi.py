45678335766beec2d9dc401aa214147a72781181
Ryan Nolan <ryannolan_@hotmail.com>
2024-09-10 07:28:11 +0000

    Update file names and settings with sports sole

--- sports_sole/wsgi.py
+++ sports_sole/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PP5 project.
+WSGI config for Sports Sole project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PP5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sports_sole.settings')
 
 application = get_wsgi_application()
