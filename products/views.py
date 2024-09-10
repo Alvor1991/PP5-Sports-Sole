3c8b4b12e9b698454e72313c4e6c04eb3e35a31b
Ryan Nolan <ryannolan_@hotmail.com>
2024-09-10 14:37:48 +0000

    Ensure product details funcionality

--- products/views.py
+++ products/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product
 
-# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
