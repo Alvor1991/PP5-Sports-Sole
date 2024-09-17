1b5cbdac3cfd6862b9d064229a68badd71572bcd
Ryan Nolan <ryannolan_@hotmail.com>
2024-09-17 14:36:30 +0000

    Add images to media, add gender to model, remove image url from model

--- products/models.py
+++ products/models.py
@@ -34,8 +34,8 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=[('Men', 'Men'), ('Women', 'Women')], default='Unisex')
 
     def __str__(self):
         """
