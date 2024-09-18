b480f48c453a045912da42a1e13a8a4fa3e812cf
Ryan Nolan <ryannolan_@hotmail.com>
2024-09-18 14:15:03 +0000

    Add meta info for seo, add rating to model and admin

--- products/models.py
+++ products/models.py
@@ -34,6 +34,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     detail_image = models.ImageField(upload_to='products/', null=True, blank=True)
     gender = models.CharField(max_length=10, choices=[('Men', 'Men'), ('Women', 'Women')], default='Unisex')
