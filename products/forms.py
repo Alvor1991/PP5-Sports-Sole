67887d61c2db0973503957b3b1237237a7dc65ec
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-02 13:41:25 +0000

    Delete products

--- products/forms.py
+++ products/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from .models import Product, Category
 
-
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
-        fields = '__all__'
+        # Exclude the sizes field from the form
+        exclude = ['sizes']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
