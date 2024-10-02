ef46cd59d41b161bc03db8517e24013a65e93a85
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-02 18:13:16 +0000

    deployment

--- sports_sole/settings.py
+++ sports_sole/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-s4nm71j8qcr7er!g3ctvpdydl$f3k!sdw!dq9oh1p&axf-6(o6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-alvor1991-pp5-m2hkfsjl009.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-alvor1991-pp5-m2hkfsjl009.ws.codeinstitute-ide.net', 'sports-sole.herokuapp.com', 'localhost']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-alvor1991-pp5-m2hkfsjl009.ws.codeinstitute-ide.net'
@@ -126,15 +126,21 @@ WSGI_APPLICATION = 'sports_sole.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 DATABASES = {
-     'default': dj_database_url.parse('your-database-url-here')
+     'default': dj_database_url.parse('postgres://uuacmhqx1aq:7RUQCvXLTRS0@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/dish_barge_polar_235696')
  }
 
 
