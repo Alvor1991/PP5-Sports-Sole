20b44736bae21ef7a648a1c905faacd3648c1efc
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-05 09:31:23 +0000

    Update static files

--- sports_sole/settings.py
+++ sports_sole/settings.py
@@ -186,6 +186,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+if not 'USE_AWS' in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
