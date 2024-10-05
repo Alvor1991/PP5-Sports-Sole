b9887bd648936a26996a4e35541bb42616671323
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-05 09:36:01 +0000

    Update static files

--- sports_sole/settings.py
+++ sports_sole/settings.py
@@ -184,11 +184,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+# This will collect static files into this directory in production
 if not 'USE_AWS' in os.environ:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
