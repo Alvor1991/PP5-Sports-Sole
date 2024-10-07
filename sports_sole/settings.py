e2471e083d0a8cbdb6ac8ec1b1a6af1b704bdafc
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-07 16:17:19 +0000

    Remove security settings

--- sports_sole/settings.py
+++ sports_sole/settings.py
@@ -217,13 +217,3 @@ DEFAULT_FROM_EMAIL = 'sportssole@example.com'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Security settings for production
-if not DEBUG:
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SECURE_HSTS_SECONDS = 31536000  # 1 year
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
