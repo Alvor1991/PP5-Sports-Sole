a09213488375fdde5fd76dbbfafdafd44ff12f53
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-07 15:50:02 +0000

    Set debug true

--- sports_sole/settings.py
+++ sports_sole/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alvor1991-pp5-m2hkfsjl009.ws.codeinstitute-ide.net', 'sports-sole-d03df7b3c157.herokuapp.com', 'localhost', '127.0.0.1']
 
