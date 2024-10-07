f51e1a3411c6ef49fa0eb7c907b701fc9907f646
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-07 16:04:41 +0000

    Add cloundianry debugging

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
-    path('debug-cloudinary/', views.debug_cloudinary, name='debug_cloudinary'),
-]
+     path('debug-cloudinary/', views.debug_cloudinary, name='debug_cloudinary'),
 ]
