f1cb88a0da157c4bd96e5b450f397b5297af76f9
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-07 20:57:49 +0000

    Update apps py files, add url to images field

--- products/models.py
+++ products/models.py
@@ -50,7 +50,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     detail_image = models.ImageField(upload_to='products/', null=True, blank=True)
     gender = models.CharField(max_length=6, choices=GENDER_CHOICES, null=True, blank=True)
     sizes = models.ManyToManyField(Size, related_name='products')
