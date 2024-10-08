cf6b130b3b369facbcbb605f10ba8642ffc0fec1
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-08 10:35:37 +0000

    Add widgets file, update settings for deployment

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -72,7 +72,7 @@ class StripeWH_Handler:
                 profile.default_street_address1 = shipping_details.address.line1
                 profile.default_street_address2 = shipping_details.address.line2
                 profile.default_county = shipping_details.address.state
-                profile.save()        
+                profile.save()
 
         order_exists = False
         attempt = 1
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['STRIPE_PUBLIC_KEY'] = "pk_test_51Q2qxaIkVV5oP7rvOc9Z0gyGJVaFhWg7d2dh9Y0A9ChbymqLE6bVfIhUrkiuPq1UXN86LBcj3NISxgr0ZzOqVK0u00zQLrREmN"
+os.environ['STRIPE_SECRET_KEY'] = "sk_test_51Q2qxaIkVV5oP7rvVe96I98Ly0vvq5ACwThn7vxi4eAPrB23hJlVw7OGPXJeeoQM93WKhpduszLBvZp046cvv1c300jbydAvZA"
+os.environ['STRIPE_WH_SECRET'] = "whsec_vF8PE95ElQodr4VpuXAPWx1FEUI2kkIY"
