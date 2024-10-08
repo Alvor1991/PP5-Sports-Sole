db39f8e6c345b81c3057fbd26e71685222f00890
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-08 17:48:27 +0000

    Update database settings to use environment variables

--- sports_sole/settings.py
+++ sports_sole/settings.py
@@ -147,7 +147,8 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+    
+  
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -198,6 +199,8 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+MEDIA_URL = '/media/'
 
 # Stripe Settings
 FREE_DELIVERY_THRESHOLD = 100
