cdcb34166edde438a9de75a8ea5e0684e0e1ac1f
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-08 18:38:41 +0000

    Add sizes to admin form

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Size
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -10,10 +10,11 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
         'image',
-        'detail_image', 
+        'detail_image',
     )
-
     ordering = ('sku',)
+    filter_horizontal = ('sizes',)  # This allows you to select sizes for products in a more user-friendly manner
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
@@ -21,5 +22,14 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = (
+        'size',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Size, SizeAdmin)  # Register Size in the admin
+
