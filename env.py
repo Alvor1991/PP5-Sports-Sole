60c3d45abc2b064b97ed88a420b9945bc8bbbc7c
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-09 14:45:58 +0000

    Update stripe key

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import os
 
 os.environ['STRIPE_PUBLIC_KEY'] = "pk_test_51Q2qxaIkVV5oP7rvOc9Z0gyGJVaFhWg7d2dh9Y0A9ChbymqLE6bVfIhUrkiuPq1UXN86LBcj3NISxgr0ZzOqVK0u00zQLrREmN"
 os.environ['STRIPE_SECRET_KEY'] = "sk_test_51Q2qxaIkVV5oP7rvVe96I98Ly0vvq5ACwThn7vxi4eAPrB23hJlVw7OGPXJeeoQM93WKhpduszLBvZp046cvv1c300jbydAvZA"
-os.environ['STRIPE_WH_SECRET'] = "whsec_VNVrg7dRKhqFIDDVvNO0k7n7BXnbLt8F"
+os.environ['STRIPE_WH_SECRET'] = "whsec_fH7MmE9PbWEvpPeeJfAx0W30P3BOHXXK"
 os.environ['SECRET_KEY'] = ' -+-q*xz=y66ev_g)-ae$qi6ny-+a*%tin754%22iu^($2ddmst'
 
 os.environ['CLOUD_NAME'] = 'dn3hzyiqk'
