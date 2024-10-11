14bd4161e664e8f9b8a6ed3e8fb71bcb3f1050b3
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-11 11:06:59 +0000

    Add newsletter signup model

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('newsletter-signup/', views.newsletter_signup, name='newsletter_signup'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewsletterSignupForm
 
 def index(request):
     """ A view to return the index page """
+    if request.method == 'POST':
+        form = NewsletterSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for subscribing to our newsletter!')
+            return redirect('home')
+    else:
+        form = NewsletterSignupForm()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'home/index.html', context)
 
-    return render(request, 'home/index.html')
+def newsletter_signup(request):
+    if request.method == 'POST':
+        form = NewsletterSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for subscribing to our newsletter!')
+            return redirect('home')
+    else:
+        form = NewsletterSignupForm()
+    return render(request, 'home/newsletter_signup.html', {'form': form})
