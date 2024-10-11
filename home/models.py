80c5b48637d329216a6a9991d91695ad052fea12
Ryan Nolan <ryannolan_@hotmail.com>
2024-10-11 13:08:56 +0000

    Add testimonial model

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class NewsletterSubscriber(models.Model):
     email = models.EmailField(unique=True)
@@ -7,3 +8,15 @@ class NewsletterSubscriber(models.Model):
 
     def __str__(self):
         return self.email
+
+class CustomerTestimonial(models.Model):
+    name = models.CharField(max_length=100)
+    testimonial = models.TextField()
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        help_text="Rating from 1 to 5"
+    )
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.rating} stars"
